2b32c03e321b3dd1e7a48e89ef07549e99440ef8
Simon Mika <simon@mika.se>
2018-07-27 12:33:36 +0200

    Removed HTTPS redirection.

--- mockup/Startup.cs
+++ mockup/Startup.cs
@@ -19,29 +19,20 @@ namespace Cogneco.WebApiTester.Mockup
 				{
 						Configuration = configuration;
 				}
-
 				public IConfiguration Configuration { get; }
-
 				// This method gets called by the runtime. Use this method to add services to the container.
 				public void ConfigureServices(IServiceCollection services)
 				{
 						services.AddSingleton<Services.ValuesService, Services.ValuesService>();
 						services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 				}
-
 				// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 				public void Configure(IApplicationBuilder app, IHostingEnvironment env)
 				{
 						if (env.IsDevelopment())
-						{
 								app.UseDeveloperExceptionPage();
-						}
 						else
-						{
 								app.UseHsts();
-						}
-
-						app.UseHttpsRedirection();
 						app.UseMvc();
 				}
 		}
